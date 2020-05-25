fec45d9fa4c5d301931d9237e2e2fa3919e2847b
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-25 12:41:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,10 +9,12 @@ end
 
 
 
+chocolates = ["snickers","Kit-Kat","Skittles",34]
+b = [chocolates, 23, 67,98]
 def using_flatten(array)
   array.flatten
 end
-
+p using_flatten(b)
 
 
 def using_delete_at(array,integer)
