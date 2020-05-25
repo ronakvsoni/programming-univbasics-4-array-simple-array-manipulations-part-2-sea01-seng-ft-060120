2853c811e990f8d4719914d39c0cd1aa2812228f
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-25 12:32:39 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+
+def using_delete_at(array,integer)
+  array.delete_at(integer)
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_insert(array,element)
+  array.insert(4,element)
+end
+
+def using_concat(array,new_array)
+  array.concat(new_array)
+end
